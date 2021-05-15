f97404c250d254f6c428f371bc3739a545c79864
Joseph Murillo <josephmurillo2425@gmail.com>
2021-05-15 11:46:42 -0600

    list users zoom api success

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,29 @@
 class HomeController < ApplicationController
   
   def index
-    require 'zoom'
 
-    Zoom.configure do |c|
-      c.api_key = Rails.application.credentials.zoom[:api_key]
-      c.api_secret = Rails.application.credentials.zoom[:api_secret]
-    end
+    api_key = Rails.application.credentials.zoom[:api_key]  
+    api_secret = Rails.application.credentials.zoom[:api_secret]
+    
+    payload = {
+      iss: api_key,
+      exp: 1.hour.from_now.to_i
+    }
+
+    token = JWT.encode(payload, api_secret, "HS256", { typ: 'JWT' })
+
+    url = URI("https://api.zoom.us/v2/users?status=active&page_size=30&page_number=1")
+
+    https = Net::HTTP.new(url.host, url.port)
+    https.use_ssl = true
+    
+    request = Net::HTTP::Get.new(url)
+    request["authorization"] = "Bearer #{token}"
+    request["content-type"] = 'application/json'
+
+    response = https.request(request)
+    puts "response.read_body"
+    puts response.read_body
     
   end
 
